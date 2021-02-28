c8623d26ea447aac600ebd368303b000f2224a72
PiotrBajko <piotr.bajko1998@gmail.com>
2021-02-28 15:11:13 +0100

    Login with access token

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,16 +1,34 @@
-import { Injectable, Body } from '@nestjs/common';
-import {UsersService} from '../users/users.service'
+import { Injectable , Body} from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
 import {CreateValidationDto} from './create-validation.dto'
+import { Console, exception } from 'console';
 
 @Injectable()
 export class AuthService {
-    constructor(private usersService:UsersService){}
+  constructor(
+    private usersService: UsersService,
+    private jwtService: JwtService
+  ) {}
 
-    async validateUser(@Body() createValidationDto: CreateValidationDto){
-        const user = this.usersService.findUser(createValidationDto.username)
-        if (user && (await user).password === createValidationDto.password){
-            return user;
-        }
-        return null;
+  async validateUser(@Body() createValidationDto: CreateValidationDto){
+    const user = await this.usersService.findUser(createValidationDto.username)
+    if (user &&  user.password === createValidationDto.password){
+        return user;
     }
+    return null;
+}
+
+  async login(@Body() createValidationDto: CreateValidationDto) {
+    const user = await this.validateUser(createValidationDto)
+    if(!user){
+      return  {
+        error: "Wrong username or password"
+      }
+    }
+    const payload = { username: user.username, sub: user.id };
+    return {
+      access_token: this.jwtService.sign(payload),
+    };
+  }
 }
